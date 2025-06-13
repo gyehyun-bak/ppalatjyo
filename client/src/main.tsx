6bfb21fdb15b610a1acf608ede4139e6a60154d0
Gyehyun Park <gyehyunbak@gmail.com>
2025-06-13 09:53:02 +0900

    fix: vitest.config.ts -> environment: 'jsdom' 추가

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+/* v8 ignore if -- @preserve */
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -3,5 +3,13 @@ import {defineConfig} from 'vitest/config'
 export default defineConfig({
     test: {
         globals: true,
+        environment: 'jsdom',
+        coverage: {
+            provider: 'v8',
+            exclude: [
+                'src/main.tsx',
+                '**/*.d.ts',      // 모든 타입 선언 파일 제외
+            ]
+        }
     },
 })
