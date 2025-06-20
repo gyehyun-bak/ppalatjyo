02d5f4987fcdf7da5522dfa19d889930bd40d4c7
Gyehyun Park <gyehyunbak@gmail.com>
2025-06-21 00:08:47 +0900

    feature: GameEventService, GameEventServiceTest 작성, MessageRepository 생성

--- server/src/test/java/ppalatjyo/server/gameevent/GameEventServiceTest.java
+++ server/src/test/java/ppalatjyo/server/gameevent/GameEventServiceTest.java
@@ -13,9 +13,13 @@ import ppalatjyo.server.gameevent.domain.GameEvent;
 import ppalatjyo.server.gameevent.domain.GameEventType;
 import ppalatjyo.server.lobby.domain.Lobby;
 import ppalatjyo.server.lobby.domain.LobbyOptions;
+import ppalatjyo.server.message.Message;
+import ppalatjyo.server.message.MessageRepository;
 import ppalatjyo.server.quiz.domain.Question;
 import ppalatjyo.server.quiz.domain.Quiz;
 import ppalatjyo.server.user.domain.User;
+import ppalatjyo.server.usergame.UserGame;
+import ppalatjyo.server.usergame.UserGameRepository;
 
 import java.util.Optional;
 
@@ -30,6 +34,12 @@ class GameEventServiceTest {
 
     @Mock
     private GameRepository gameRepository;
+
+    @Mock
+    private UserGameRepository userGameRepository;
+
+    @Mock
+    private MessageRepository messageRepository;
 
     @InjectMocks
     private GameEventService gameEventService;
@@ -57,6 +67,124 @@ class GameEventServiceTest {
         assertThat(gameEvent).isNotNull();
         assertThat(gameEvent.getGame()).isEqualTo(game);
         assertThat(gameEvent.getType()).isEqualTo(GameEventType.GAME_STARTED);
+    }
+
+    @Test
+    @DisplayName("게임 종료")
+    void ended() {
+        // given
+        Quiz quiz = Quiz.createQuiz("quiz", User.createMember("host", null, null));
+        Question.create(quiz, "question");
+        Lobby lobby = Lobby.createLobby("lobby", User.createGuest("user"), quiz, LobbyOptions.defaultOptions());
+
+        Long gameId = 1L;
+        Game game = Game.start(lobby);
+        when(gameRepository.findById(gameId)).thenReturn(Optional.of(game));
+
+        // when
+        gameEventService.ended(gameId);
+
+        // then
+        ArgumentCaptor<GameEvent> captor = ArgumentCaptor.forClass(GameEvent.class);
+        verify(gameEventRepository, times(1)).save(captor.capture());
+        GameEvent gameEvent = captor.getValue();
+        assertThat(gameEvent).isNotNull();
+        assertThat(gameEvent.getGame()).isEqualTo(game);
+        assertThat(gameEvent.getType()).isEqualTo(GameEventType.GAME_ENDED);
+    }
+
+    @Test
+    @DisplayName("타임 아웃")
+    void timeOut() {
+        // given
+        Quiz quiz = Quiz.createQuiz("quiz", User.createMember("host", null, null));
+        Question.create(quiz, "question");
+        Lobby lobby = Lobby.createLobby("lobby", User.createGuest("user"), quiz, LobbyOptions.defaultOptions());
+
+        Long gameId = 1L;
+        Game game = Game.start(lobby);
+        when(gameRepository.findById(gameId)).thenReturn(Optional.of(game));
+
+        // when
+        gameEventService.timeOut(gameId);
+
+        // then
+        ArgumentCaptor<GameEvent> captor = ArgumentCaptor.forClass(GameEvent.class);
+        verify(gameEventRepository, times(1)).save(captor.capture());
+        GameEvent gameEvent = captor.getValue();
+        assertThat(gameEvent).isNotNull();
+        assertThat(gameEvent.getGame()).isEqualTo(game);
+        assertThat(gameEvent.getType()).isEqualTo(GameEventType.TIME_OUT);
+    }
+
+    @Test
+    @DisplayName("정답")
+    void rightAnswer() {
+        // given
+        User user = User.createMember("host", null, null);
+        Quiz quiz = Quiz.createQuiz("quiz", user);
+        Question.create(quiz, "question");
+        Lobby lobby = Lobby.createLobby("lobby", User.createGuest("user"), quiz, LobbyOptions.defaultOptions());
+
+        Long gameId = 1L;
+        Game game = Game.start(lobby);
+
+        Long userGameId = 1L;
+        UserGame userGame = game.getUserGames().getFirst();
+
+        Long messageId = 1L;
+        Message message = Message.create("message", user, lobby);
+        when(gameRepository.findById(gameId)).thenReturn(Optional.of(game));
+        when(userGameRepository.findById(userGameId)).thenReturn(Optional.of(userGame));
+        when(messageRepository.findById(messageId)).thenReturn(Optional.of(message));
+
+        // when
+        gameEventService.rightAnswer(gameId, userGameId, messageId);
+
+        // then
+        ArgumentCaptor<GameEvent> captor = ArgumentCaptor.forClass(GameEvent.class);
+        verify(gameEventRepository, times(1)).save(captor.capture());
+        GameEvent gameEvent = captor.getValue();
+        assertThat(gameEvent).isNotNull();
+        assertThat(gameEvent.getGame()).isEqualTo(game);
+        assertThat(gameEvent.getType()).isEqualTo(GameEventType.RIGHT_ANSWER);
+        assertThat(gameEvent.getMessage()).isEqualTo(message);
+        assertThat(gameEvent.getUserGame()).isEqualTo(userGame);
+    }
+
+    @Test
+    @DisplayName("오답")
+    void wrongAnswer() {
+        // given
+        User user = User.createMember("host", null, null);
+        Quiz quiz = Quiz.createQuiz("quiz", user);
+        Question.create(quiz, "question");
+        Lobby lobby = Lobby.createLobby("lobby", User.createGuest("user"), quiz, LobbyOptions.defaultOptions());
+
+        Long gameId = 1L;
+        Game game = Game.start(lobby);
+
+        Long userGameId = 1L;
+        UserGame userGame = game.getUserGames().getFirst();
+
+        Long messageId = 1L;
+        Message message = Message.create("message", user, lobby);
+        when(gameRepository.findById(gameId)).thenReturn(Optional.of(game));
+        when(userGameRepository.findById(userGameId)).thenReturn(Optional.of(userGame));
+        when(messageRepository.findById(messageId)).thenReturn(Optional.of(message));
+
+        // when
+        gameEventService.wrongAnswer(gameId, userGameId, messageId);
+
+        // then
+        ArgumentCaptor<GameEvent> captor = ArgumentCaptor.forClass(GameEvent.class);
+        verify(gameEventRepository, times(1)).save(captor.capture());
+        GameEvent gameEvent = captor.getValue();
+        assertThat(gameEvent).isNotNull();
+        assertThat(gameEvent.getGame()).isEqualTo(game);
+        assertThat(gameEvent.getType()).isEqualTo(GameEventType.WRONG_ANSWER);
+        assertThat(gameEvent.getMessage()).isEqualTo(message);
+        assertThat(gameEvent.getUserGame()).isEqualTo(userGame);
     }
 
 
