4aed18186899ab5d2a7585b61c5c93781ae9cda6
Gyehyun Park <gyehyunbak@gmail.com>
2025-06-24 16:13:45 +0900

    feature: main.tsx에 QueryClientProvider 추가

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 /* v8 ignore if -- @preserve */
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
