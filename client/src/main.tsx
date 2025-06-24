4e3f9668b0da50b923112646a2d776b56b48f07c
Gyehyun Park <167384362+gyehyun-bak@users.noreply.github.com>
2025-06-24 12:55:00 +0900

    [FEATURE #46]: 페이지 라우팅 구현 (#79)
    * feature: react-router:7.6.2 설치
    
    * feature: main.tsx에서 BrowserRouter, Routes, Route 등 react-router을 declarative로 설정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router';
 /* v8 ignore if -- @preserve */
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
