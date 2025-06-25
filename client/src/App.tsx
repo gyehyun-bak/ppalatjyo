44eaaa0f9cc53c8ece8b22f0202168edff10c1fa
Gyehyun Park <gyehyunbak@gmail.com>
2025-06-25 15:42:18 +0900

    feature: Landing.tsx에 대해 BDD로 테스트 작성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
-import { Routes, useNavigate, useHref } from 'react-router';
+import { Routes, useNavigate, useHref, Route } from 'react-router';
 import './App.css';
 import { HeroUIProvider } from '@heroui/react';
+import Landing from './page/Landing';
 
 function App() {
     const navigate = useNavigate();
 
     return (
         <HeroUIProvider navigate={navigate} useHref={useHref}>
-            <Routes></Routes>
+            <Routes>
+                <Route path="/landing" element={<Landing />} />
+            </Routes>
         </HeroUIProvider>
     );
 }
