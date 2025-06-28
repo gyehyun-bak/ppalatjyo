9ee81ec978d9dbaa2cfcc4deb2de0ab794a49e43
Gyehyun Park <gyehyunbak@gmail.com>
2025-06-28 22:44:36 +0900

    test: Landing.tsx 테스트 작성

--- client/src/mocks/server.ts
+++ client/src/mocks/server.ts
@@ -2,3 +2,4 @@ import { setupServer } from "msw/node";
 import { handlers } from "./handlers.js";
 
 export const server = setupServer(...handlers);
+export const baseUrl = "http://localhost:8080/api";
--- client/src/test/page/Landing.test.tsx
+++ client/src/test/page/Landing.test.tsx
@@ -4,10 +4,40 @@ import { renderWithWrapper } from "../utils/renderWithWrapper";
 import "@testing-library/jest-dom";
 import { screen, waitFor } from "@testing-library/react";
 import { it, expect, describe } from "vitest";
+import { baseUrl, server } from "../../mocks/server";
+import { http, HttpResponse } from "msw";
+import type { JoinAsGuestRequestDto } from "../../types/api/auth/JoinAsGuestRequestDto";
+import type { ResponseDto } from "../../types/api/ResponseDto";
+import type { JoinAsGuestResponseDto } from "../../types/api/auth/JoinAsGuestResponseDto";
+
+const { mockNavigate, mockAddToast } = vi.hoisted(() => ({
+    mockNavigate: vi.fn(),
+    mockAddToast: vi.fn(),
+}));
+
+vi.mock("react-router", async () => {
+    const actual = await vi.importActual<typeof import("react-router")>(
+        "react-router"
+    );
+    return {
+        ...actual,
+        useNavigate: () => mockNavigate,
+    };
+});
+
+vi.mock("@heroui/react", async () => {
+    const actual = await vi.importActual<typeof import("@heroui/react")>(
+        "@heroui/react"
+    );
+    return {
+        ...actual,
+        addToast: mockAddToast,
+    };
+});
 
 describe("Landing", () => {
     afterEach(() => {
-        vi.resetAllMocks();
+        vi.clearAllMocks();
     });
 
     it("닉네임 인풋이 렌더링 된다", () => {
@@ -32,8 +62,24 @@ describe("Landing", () => {
         expect(button).toBeInTheDocument();
     });
 
-    it('닉네임 입력 후 "계속하기" 클릭 시 게스트 가입 요청을 보낸다', async () => {
+    it("닉네임을 입력하고 버튼을 눌러 가입 성공 시 엑세스 토큰을 저장하고 /home으로 navigate 된다", async () => {
         // given
+        server.use(
+            http.post<
+                never,
+                JoinAsGuestRequestDto,
+                ResponseDto<JoinAsGuestResponseDto>
+            >(baseUrl + "/auth/sign-up/guest", async () => {
+                return HttpResponse.json({
+                    success: true,
+                    status: 200,
+                    data: {
+                        accessToken: "mock-access-token",
+                    },
+                });
+            })
+        );
+
         renderWithWrapper(<Landing />);
 
         const input = screen.getByLabelText("닉네임");
@@ -46,20 +92,51 @@ describe("Landing", () => {
         await user.click(button);
 
         // then
+        await waitFor(() =>
+            expect(localStorage.getItem("accessToken")).toBe(
+                "mock-access-token"
+            )
+        );
         await waitFor(() => {
-            // expect(reactQuery.useMutation).toHaveBeenCalledWith({ nickname: "hello" });
+            expect(mockNavigate).toHaveBeenCalledWith("/home");
         });
-    });
-
-    it("가입 성공 시 accessToken을 받고 /home으로 이동한다", async () => {
-        // given
-        // when
-        // then
     });
 
     it("오류 응답이 반환되면 오류 토스트를 띄운다", async () => {
         // given
+        server.use(
+            http.post<
+                never,
+                JoinAsGuestRequestDto,
+                ResponseDto<JoinAsGuestResponseDto>
+            >(baseUrl + "/auth/sign-up/guest", async () => {
+                return HttpResponse.json(
+                    {
+                        success: true,
+                        status: 500,
+                        data: {
+                            accessToken: "mock-access-token",
+                        },
+                    },
+                    { status: 500 }
+                );
+            })
+        );
+
+        renderWithWrapper(<Landing />);
+
+        const input = screen.getByLabelText("닉네임");
+        const button = screen.getByRole("button", { name: "계속하기" });
+
+        const user = userEvent.setup();
+
         // when
+        await user.type(input, "error");
+        await user.click(button);
+
         // then
+        await waitFor(() => {
+            expect(mockAddToast).toHaveBeenCalled();
+        });
     });
 });
