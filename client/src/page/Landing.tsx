c8806ccadd57b2640acd7729949b9b9d84718327
Gyehyun Park <gyehyunbak@gmail.com>
2025-06-28 16:18:58 +0900

    feature: Landing.tsx에 Input 추가

--- client/src/page/Landing.tsx
+++ client/src/page/Landing.tsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+import { Input } from "@heroui/react";
+
 export default function Landing() {
-    return <div>landing</div>;
+    const [nickname, setNickname] = useState("");
+
+    return (
+        <div>
+            <h1>Landing</h1>
+            <Input
+                label="닉네임"
+                type="string"
+                isClearable
+                value={nickname}
+                onChange={(e) => setNickname(e.target.value)}
+                onClear={() => setNickname("")}
+            />
+        </div>
+    );
 }
