c5906b26bef67c5a85430a447ad414b8e7742f63
Gyehyun Park <gyehyunbak@gmail.com>
2025-06-29 15:55:37 +0900

    refactor: HomeHeader.tsx를 HomeLayout으로 통합

--- client/src/components/HomeHeader.tsx
+++
@@ -1,9 +0,0 @@
-import { Navbar, NavbarBrand } from '@heroui/react';
-
-export default function HomeHeader() {
-    return (
-        <Navbar>
-            <NavbarBrand>빨랐죠</NavbarBrand>
-        </Navbar>
-    );
-}
--- client/src/layout/HomeLayout.tsx
+++ client/src/layout/HomeLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router';
-import HomeHeader from '../components/HomeHeader';
+import { Navbar, NavbarBrand } from '@heroui/react';
 
 export default function HomeLayout() {
     return (
         <div className="flex flex-col size-full">
-            <HomeHeader />
+            <Navbar>
+                <NavbarBrand>빨랐죠</NavbarBrand>
+            </Navbar>
             <Outlet />
         </div>
     );
