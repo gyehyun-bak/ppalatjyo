4df77a2d5034e9fae3161e182ca9c2ac511146ae
Gyehyun Park <gyehyunbak@gmail.com>
2025-06-29 15:04:12 +0900

    feature: Home.tsx, HomeLayout.tsx 추가, 라우팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,17 @@ import { Routes, Route } from 'react-router';
 import './App.css';
 import Landing from './page/Landing';
 import RootLayout from './layout/RootLayout';
+import Home from './page/Home';
+import HomeLayout from './layout/HomeLayout';
 
 function App() {
     return (
         <Routes>
             <Route element={<RootLayout />}>
                 <Route path="/landing" element={<Landing />} />
+                <Route element={<HomeLayout />}>
+                    <Route path="/home" element={<Home />} />
+                </Route>
             </Route>
         </Routes>
     );
