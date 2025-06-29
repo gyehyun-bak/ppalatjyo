e9d138a5fea90b72fc0cb6d8cb384c6dc3c6dc7c
Gyehyun Park <gyehyunbak@gmail.com>
2025-06-29 16:01:10 +0900

    feature: Primary 컬러 테마 설정: #FFBF00

--- client/src/layout/HomeLayout.tsx
+++ client/src/layout/HomeLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router';
 import { Navbar, NavbarBrand } from '@heroui/react';
+import Logo from '../components/Logo';
 
 export default function HomeLayout() {
     return (
         <div className="flex flex-col size-full">
             <Navbar>
-                <NavbarBrand>빨랐죠</NavbarBrand>
+                <NavbarBrand>
+                    <span>빨랐죠</span>
+                    <Logo size={22} />
+                </NavbarBrand>
             </Navbar>
             <Outlet />
         </div>
