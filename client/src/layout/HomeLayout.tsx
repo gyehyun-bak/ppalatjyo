200401db54354549c084563fcb368ee2a6a94618
Gyehyun Park <gyehyunbak@gmail.com>
2025-06-29 21:01:03 +0900

    feature: HomeLayout 헤더에 border 추가

--- client/src/layout/HomeLayout.tsx
+++ client/src/layout/HomeLayout.tsx
@@ -1,11 +1,11 @@
-import { Outlet } from 'react-router';
-import { Navbar, NavbarBrand } from '@heroui/react';
-import Logo from '../components/Logo';
+import { Outlet } from "react-router";
+import { Navbar, NavbarBrand } from "@heroui/react";
+import Logo from "../components/Logo";
 
 export default function HomeLayout() {
     return (
         <div className="flex flex-col size-full">
-            <Navbar>
+            <Navbar isBordered>
                 <NavbarBrand>
                     <span className="text-primary font-bold">빨랐죠</span>
                     <Logo size={22} />
