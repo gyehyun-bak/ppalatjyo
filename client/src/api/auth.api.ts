bf6fc4af1d2eddf4b9563610aeaad5d63a0d91cd
Gyehyun Park <gyehyunbak@gmail.com>
2025-06-30 10:27:15 +0900

    feature: AuthenticationLayout 구현

--- client/src/api/auth.api.ts
+++ client/src/api/auth.api.ts
@@ -1,15 +1,23 @@
-import type { JoinAsGuestRequestDto } from "../types/api/auth/JoinAsGuestRequestDto";
-import type { JoinAsGuestResponseDto } from "../types/api/auth/JoinAsGuestResponseDto";
-import type { ResponseDto } from "../types/api/ResponseDto";
-import { api } from "./axios";
+import type { JoinAsGuestRequestDto } from '../types/api/auth/JoinAsGuestRequestDto';
+import type { JoinAsGuestResponseDto } from '../types/api/auth/JoinAsGuestResponseDto';
+import type { TokenReissueResponseDto } from '../types/api/auth/TokenReissueResponseDto';
+import type { ResponseDto } from '../types/api/ResponseDto';
+import { api } from './axios';
 
 export const postSignUpGuest = async (
     requestDto: JoinAsGuestRequestDto
 ): Promise<ResponseDto<JoinAsGuestResponseDto>> => {
     return (
         await api.post<ResponseDto<JoinAsGuestResponseDto>>(
-            "/auth/sign-up/guest",
+            '/auth/sign-up/guest',
             requestDto
         )
     ).data;
+};
+
+export const getNewTokens = async (): Promise<
+    ResponseDto<TokenReissueResponseDto>
+> => {
+    return (await api.get<ResponseDto<JoinAsGuestResponseDto>>('/auth/tokens'))
+        .data;
 };
--- client/src/layout/AuthenticationLayout.tsx
+++ client/src/layout/AuthenticationLayout.tsx
@@ -1,5 +1,28 @@
 import { Outlet } from 'react-router';
+import { useAuthStore } from '../store/useAuthStore';
+import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { getNewTokens } from '../api/auth.api';
 
+/**
+ * 인증 과정을 처리하는 레이아웃 컴포넌트입니다.
+ * 토큰 재발급을 시도하고, 성공하면 인증 상태를 업데이트합니다.
+ */
 export default function AuthenticationLayout() {
+    const { setAuthenticated } = useAuthStore();
+
+    const { data, isSuccess } = useQuery({
+        queryKey: ['tokens'],
+        queryFn: getNewTokens,
+        retry: false,
+    });
+
+    useEffect(() => {
+        if (isSuccess && data.data) {
+            setAuthenticated(true);
+            localStorage.setItem('accessToken', data.data.accessToken);
+        }
+    }, [isSuccess, data, setAuthenticated]);
+
     return <Outlet />;
 }
