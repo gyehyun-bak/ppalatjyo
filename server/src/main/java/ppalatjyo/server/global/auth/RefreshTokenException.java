df5bd5c039d1ec6f3477ff02be66a6f3c7a357b6
Gyehyun Park <167384362+gyehyun-bak@users.noreply.github.com>
2025-06-30 10:58:13 +0900

    [REFACTOR #101]: 토큰 재발행 요청 시 RefreshToken이 없는 경우 403 Forbidden 반환하도록 수정 (#102)

---
+++ server/src/main/java/ppalatjyo/server/global/auth/RefreshTokenException.java
@@ -0,0 +1,7 @@
+package ppalatjyo.server.global.auth;
+
+public class RefreshTokenException extends RuntimeException {
+    public RefreshTokenException(String message) {
+        super(message);
+    }
+}
