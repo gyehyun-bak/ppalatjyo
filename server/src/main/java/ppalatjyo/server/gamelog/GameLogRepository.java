8dcb5fdc860a6ed416770c6cb7d577767b1cc4e9
Gyehyun Park <167384362+gyehyun-bak@users.noreply.github.com>
2025-07-01 09:27:04 +0900

    [REFACTOR #110]: GameLog 도메인 제거 (#112)

--- server/src/main/java/ppalatjyo/server/gamelog/GameLogRepository.java
+++
@@ -1,7 +0,0 @@
-package ppalatjyo.server.gamelog;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ppalatjyo.server.gamelog.domain.GameLog;
-
-public interface GameLogRepository extends JpaRepository<GameLog, Long> {
-}
--- server/src/main/java/ppalatjyo/server/gamelog/domain/GameLogType.java
+++
@@ -1,5 +0,0 @@
-package ppalatjyo.server.gamelog.domain;
-
-public enum GameLogType {
-    GAME_STARTED, GAME_ENDED, WRONG_ANSWER, RIGHT_ANSWER, TIME_OUT
-}
