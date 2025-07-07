099712bdaab64ebadd40a564f9d79d8f94773254
Gyehyun Park <167384362+gyehyun-bak@users.noreply.github.com>
2025-07-07 17:36:53 +0900

    [FEATURE #134]: 문제 수정 페이지 구현 (#151)
    * test: EditQuestionPage.test 작성
    
    * feature: 페이지 마운트시 기존 퀴즈 정보 표시
    
    * feature: 저장하기 버튼 누르면 저장 요청 + 성공 시 문제 상세 보기로 이동
    
    * feature: 문제 삭제 구현

---
+++ client/src/page/question/EditQuestionPage.tsx
@@ -0,0 +1,173 @@
+import {
+    addToast,
+    Button,
+    Form,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    useDisclosure,
+} from '@heroui/react';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useNavigate, useParams } from 'react-router';
+import Input from '../../components/common/Input';
+import { useEffect, useState } from 'react';
+import {
+    deleteQuestion,
+    editQuestion,
+    getQuestion,
+} from '../../api/question.api';
+import { X } from 'lucide-react';
+import type { EditQuestionRequest } from '../../api/types/question/EditQuestionRequest';
+
+export default function EditQuestionPage() {
+    const navigate = useNavigate();
+    const [content, setContent] = useState('');
+    const [answer, setAnswer] = useState('');
+    const [answers, setAnswers] = useState<string[]>([]);
+    const { quizId, questionId } = useParams<{
+        quizId: string;
+        questionId: string;
+    }>();
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+    const { data, isSuccess } = useQuery({
+        queryKey: ['question', questionId],
+        queryFn: () =>
+            getQuestion(quizId as string, questionId as string, true),
+        enabled: !!quizId && !!questionId,
+    });
+
+    const { mutate: editMutate, isPending: editIsPending } = useMutation({
+        mutationFn: (data: EditQuestionRequest) =>
+            editQuestion(quizId as string, questionId as string, data),
+        onSuccess: () => {
+            navigate(`/quizzes/${quizId}/questions/${questionId}`);
+        },
+    });
+
+    const { mutate: deleteMutate, isPending: deleteIsPending } = useMutation({
+        mutationFn: () =>
+            deleteQuestion(quizId as string, questionId as string),
+        onSuccess: () => {
+            navigate(`/quizzes/${quizId}`);
+        },
+    });
+
+    useEffect(() => {
+        if (isSuccess && data) {
+            setContent(data.content);
+            setAnswers(data.answers.map((answer) => answer.content));
+        }
+    }, [data, isSuccess]);
+
+    const handleAddAnswer = () => {
+        if (answer.trim() === '') return;
+        setAnswers((prev) => [...prev, answer.trim()]);
+        setAnswer('');
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (answers.length == 0) {
+            addToast({ title: '최소 1개의 답이 있어야 합니다.' });
+            return;
+        }
+
+        const data: EditQuestionRequest = {
+            content,
+            answers,
+        };
+
+        editMutate(data);
+    };
+
+    return (
+        <>
+            <Form onSubmit={handleSubmit}>
+                <Input
+                    isRequired
+                    value={content}
+                    onValueChange={setContent}
+                    data-testid="content-input"
+                />
+                <Input
+                    value={answer}
+                    onValueChange={setAnswer}
+                    data-testid="answer-input"
+                />
+                <Button
+                    onPress={handleAddAnswer}
+                    data-testid="add-answer-button"
+                >
+                    추가
+                </Button>
+                <ul>
+                    {answers.map((answer, index) => (
+                        <li key={index}>
+                            <p>{answer}</p>
+                            <Button
+                                type="button"
+                                data-testid="delete-answer-button"
+                                onPress={() =>
+                                    setAnswers(
+                                        answers.filter((_, i) => i !== index)
+                                    )
+                                }
+                            >
+                                <X />
+                            </Button>
+                        </li>
+                    ))}
+                </ul>
+                <Button
+                    type="button"
+                    data-testid="delete-button"
+                    onPress={onOpen}
+                >
+                    삭제하기
+                </Button>
+                <Button
+                    isLoading={editIsPending}
+                    type="submit"
+                    data-testid="save-button"
+                >
+                    저장하기
+                </Button>
+            </Form>
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader>질문 삭제하기</ModalHeader>
+                            <ModalBody>
+                                <p>
+                                    정말로 삭제하시겠습니까? 삭제 이후에는
+                                    되돌릴 수 없습니다.
+                                </p>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button
+                                    isLoading={deleteIsPending}
+                                    onPress={onClose}
+                                >
+                                    취소하기
+                                </Button>
+                                <Button
+                                    isLoading={deleteIsPending}
+                                    color="danger"
+                                    onPress={() => deleteMutate()}
+                                    data-testid="confirm-delete-button"
+                                >
+                                    삭제하기
+                                </Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+        </>
+    );
+}
