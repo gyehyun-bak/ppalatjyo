a01c1c385415c8358ef304622d22485cdf03829f
Gyehyun Park <gyehyunbak@gmail.com>
2025-07-08 15:59:10 +0900

    test: QuestionDetailsPage.test.tsx 작성

---
+++ client/src/test/page/question/QuestionDetailsPage.test.tsx
@@ -0,0 +1,4 @@
+describe('QuestionDetailsPage', () => {
+    it('should마운트 시 쿼리 파라미터의 questionId로 퀴즈 정보 불러와 입력 및 답 목록에 표시한다', async () => {});
+    it('"수정하기" 버튼을 누르면 "문제 수정하기" 페이지로 이동한다', async () => {});
+});
